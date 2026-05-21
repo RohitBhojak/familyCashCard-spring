2e50d4324da12f83b022bf72b377d72287528cfd
Rohit Bhojak <rohitbhojak99@gmail.com>
2026-05-21 22:25:19 +0530

    Add post api

--- src/test/java/example/cashcard/CashcardApplicationTests.java
+++ src/test/java/example/cashcard/CashcardApplicationTests.java
@@ -10,6 +10,8 @@ import org.springframework.http.ResponseEntity;
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
 
+import java.net.URI;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -39,5 +41,28 @@ class CashcardApplicationTests {
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
         assertThat(response.getBody()).isBlank();
+    }
+
+    @Test
+    void shouldCreateANewCashCard() {
+        CashCard newCashCard = new CashCard(null, 250.00);
+        ResponseEntity<Void> createResponse = restTemplate.postForEntity("/cashcards", newCashCard, Void.class);
+
+        assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+
+        URI locationOfNewCashCard = createResponse.getHeaders().getLocation();
+
+        ResponseEntity<String> response = restTemplate.getForEntity(locationOfNewCashCard, String.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        DocumentContext documentContext = JsonPath.parse(response.getBody());
+
+        Number id = documentContext.read("$.id");
+        assertThat(id).isNotNull();
+
+        Double amount = documentContext.read("$.amount");
+        assertThat(amount).isEqualTo(250.00);
+
     }
 }
